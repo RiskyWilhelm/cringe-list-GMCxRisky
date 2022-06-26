35de04583acc8944373a72264d53e0499791c214
riskyWilhelm <ks7772521@gmail.com>
2022-06-26 13:05:40 +0300

    Additional Save

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@
 // Get root element and set margin of left side by button sizes
 // document.documentElement.style.setProperty("--align-relatives-fromsidenav", currentsize);
 var timeouthalf, currentSelection, clickedElement, timeout_addnote, timeout_addnoteClearAnimation, timeout_resizeBoxAnimation, timeout_resizeTextInitialization, i, notetemplate, lastnote = 1, lastContainerHeight = 0;
+// $.contains(this, event.target) is faster than the find or children. Will update soon as possible.
 var createnotetemplate = function(){
 notetemplate = $('<div>', {
     class: 'cl_note'
@@ -17,22 +18,65 @@ $('<div>', {
 }).appendTo(notetemplate);
 
 $('<textarea>', {
-    class: 'input_text cl_textarea noEnter',
+    class: 'input_text cl_textarea noEnter font-medium',
     placeholder: 'Title',
-    maxlength: '75',
-    style: 'font-size:large; margin:0; padding-right:20px;'
-}).appendTo($(notetemplate).find('.cl_note-title'));
+    maxlength: '75'
+}).appendTo($(notetemplate).children('.cl_note-title'));
 
 $('<div>', {
     class: 'cl_note-message'
 }).appendTo(notetemplate);
 
 $('<textarea>', {
-    class: 'input_text cl_textarea',
+    class: 'input_text cl_textarea font-medium',
     placeholder: 'Message',
-    maxlength: '300',
-    style: 'font-size:large; margin:0; padding-right:20px;'
-}).appendTo($(notetemplate).find('.cl_note-message'));
+    maxlength: '400',
+}).appendTo($(notetemplate).children('.cl_note-message'));
+
+$('<div>', {
+    class: 'horizontal_content',
+    style: 'justify-content: space-evenly;'
+}).append(
+  $('<button>', {
+      class: 'cl_button button_hover button_defaultsize button_mobilepadding'
+}).append(
+  $('<svg>', {
+    viewBox: "0 0 24 24",
+    xmlns: "http://www.w3.org/2000/svg",
+    fill: 'none'
+})).append(
+  $('<path>', {
+    fill: "currentColor",
+    d: "M14.8284 12L16.2426 13.4142L19.071 10.5858C20.6331 9.02365 20.6331 6.49099 19.071 4.9289C17.509 3.3668 14.9763 3.3668 13.4142 4.9289L10.5858 7.75732L12 9.17154L14.8284 6.34311C15.6095 5.56206 16.8758 5.56206 17.6568 6.34311C18.4379 7.12416 18.4379 8.39049 17.6568 9.17154L14.8284 12Z"
+})
+).append(
+  $('<path>', {
+    fill: "currentColor",
+    d:"M12 14.8285L13.4142 16.2427L10.5858 19.0711C9.02372 20.6332 6.49106 20.6332 4.92896 19.0711C3.36686 17.509 3.36686 14.9764 4.92896 13.4143L7.75739 10.5858L9.1716 12L6.34317 14.8285C5.56212 15.6095 5.56212 16.8758 6.34317 17.6569C7.12422 18.4379 8.39055 18.4379 9.1716 17.6569L12 14.8285Z"
+  })
+)).appendTo(notetemplate);
+
+
+
+// $('<svg>', {
+//   viewBox: "0 0 24 24",
+//   xmlns: "http://www.w3.org/2000/svg"
+// }).appendTo($(notetemplate).children('.horizontal_content').last().children('button').last());
+
+// $('<path>'), {
+//   fill: "currentColor",
+//   d: "M14.8284 12L16.2426 13.4142L19.071 10.5858C20.6331 9.02365 20.6331 6.49099 19.071 4.9289C17.509 3.3668 14.9763 3.3668 13.4142 4.9289L10.5858 7.75732L12 9.17154L14.8284 6.34311C15.6095 5.56206 16.8758 5.56206 17.6568 6.34311C18.4379 7.12416 18.4379 8.39049 17.6568 9.17154L14.8284 12Z"
+// }
+
+// $('<path>'), {
+//   fill: "currentColor",
+//   d:"M12 14.8285L13.4142 16.2427L10.5858 19.0711C9.02372 20.6332 6.49106 20.6332 4.92896 19.0711C3.36686 17.509 3.36686 14.9764 4.92896 13.4143L7.75739 10.5858L9.1716 12L6.34317 14.8285C5.56212 15.6095 5.56212 16.8758 6.34317 17.6569C7.12422 18.4379 8.39055 18.4379 9.1716 17.6569L12 14.8285Z"
+// }
+// $('<path>'), {
+//   fill: "currentColor",
+//   d:"M14.8285 10.5857C15.219 10.1952 15.219 9.56199 14.8285 9.17147C14.4379 8.78094 13.8048 8.78094 13.4142 9.17147L9.1716 13.4141C8.78107 13.8046 8.78107 14.4378 9.1716 14.8283C9.56212 15.2188 10.1953 15.2188 10.5858 14.8283L14.8285 10.5857Z"
+// }
+
 console.log("Note Initalization: Success");
 };
 
@@ -40,6 +84,7 @@ createnotetemplate();
 
 window.onclick=function(e){
   clickedElement = e;
+  console.log($('#justatest'));
 }
 
 window.onload=function(){
